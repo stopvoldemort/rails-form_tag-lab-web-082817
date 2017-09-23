f2bfab870af03e2ceb6fc9c03949341f50bad77e
stopvoldemort <david.squires1@gmail.com>
2017-09-23 14:00:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'David', last_name: 'Squires')
+Student.create(first_name: 'Claire', last_name: 'Kiefer')
